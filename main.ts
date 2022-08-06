57f4c23b4c95d99a9c69d5cb971b049a35d037e7
Mr Reid <64812253+sreid111@users.noreply.github.com>
2022-08-06 10:11:14 +1000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,14 +3,16 @@ datalogger.onLogFull(function () {
 })
 input.onButtonPressed(Button.AB, function () {
     datalogger.deleteLog()
+    basic.showString("Deleting data")
 })
+basic.showString("Temp logger")
 datalogger.setColumnTitles("Temp")
 loops.everyInterval(1800000, function () {
     datalogger.log(datalogger.createCV("Temp", input.temperature()))
+    led.plot(0, 0)
+    basic.pause(2000)
+    led.unplot(0, 0)
 })
 basic.forever(function () {
-    led.plot(0, 0)
-    basic.pause(5000)
-    led.unplot(0, 0)
-    basic.pause(5000)
+	
 })
